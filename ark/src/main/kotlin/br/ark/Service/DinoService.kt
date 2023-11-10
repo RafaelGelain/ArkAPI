b1e8fe49d8c5245b679a6105aa0e71222b9482e1
RafaelGelain <rafael.augustogelain2@gmail.com>
2023-11-09 21:34:16 -0300

    Começo do banco de dados.

--- ark/src/main/kotlin/br/ark/Service/DinoService.kt
+++ ark/src/main/kotlin/br/ark/Service/DinoService.kt
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service
 private const val DINO_NOT_FOUND_MESSAGE = "Dinossauro não encontrado!"
 @Service
 class DinoService (private val repository: DinoRepository, private val converter: DinoConverter) {
-    fun listar(): List<DinoResponseDTO>{
+    fun listar(): List<DinoResponseDTO>{teste
         return repository.findAll()
             .map(converter::toEventoResponseDTO)
     }
